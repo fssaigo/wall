d920f5b291e660988f12664d29f8c5ef09eded41
wangziqing <eininst@qq.com>
2016-08-09 17:36:26 +0800

    增加 分页逻辑

--- pc/js/common.js
+++ pc/js/common.js
@@ -1,9 +1,15 @@
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r!=null) return unescape(r[2]); return null; //返回参数值
+}
+
 function zanTa(e, id, num) {
-    if (!localStorage.getItem("selfNum_"+id)) {
-        localStorage.setItem("selfNum_"+id, 0);
+    if (!localStorage.getItem("selfNum_" + id)) {
+        localStorage.setItem("selfNum_" + id, 0);
     }
-    if (localStorage.getItem("selfNum_"+id) <= 0) {
-        localStorage.setItem("selfNum_"+id, ++num);
+    if (localStorage.getItem("selfNum_" + id) <= 0) {
+        localStorage.setItem("selfNum_" + id, ++num);
     } else {
         alert("你已经赞过了");
     }
