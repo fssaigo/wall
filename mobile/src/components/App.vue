02f25a30d337a57a083312dda16af039adcf888c
wangziqing <eininst@qq.com>
2016-08-10 13:04:25 +0800

    mobile构建

--- mobile/src/components/App.vue
+++ mobile/src/components/App.vue
@@ -1,47 +1,48 @@
 <template>
-  <div class="content">
+
     <div class="head">
 
+        <img src="../imgs/m-banner.jpg">
     </div>
 
     <div class="buttons-tab fixed-tab" data-offset="44">
-      <a href="#tab1" class="tab-link active button">活动说明</a>
-      <a href="#tab2" class="tab-link button">活动规则</a>
-      <a href="#tab3" class="tab-link button">奖项设置</a>
-      <a href="#tab4" class="tab-link button">作品展示</a>
+        <a href="#tab1" class="tab-link active button">活动说明</a>
+        <a href="#tab2" class="tab-link button">活动规则</a>
+        <a href="#tab3" class="tab-link button">奖项设置</a>
+        <a href="#tab4" class="tab-link button">作品展示</a>
     </div>
 
     <div class="tabs">
-      <div id="tab1" class="tab active">
-        <div class="content-block">
-            活动说明
+        <div id="tab1" class="tab active">
+            <div class="content-block">
+                活动说明
+            </div>
         </div>
-      </div>
-      <div id="tab2" class="tab">
-        <div class="content-block">
-         活动规则
+        <div id="tab2" class="tab">
+            <div class="content-block">
+                活动规则
+            </div>
         </div>
-      </div>
-      <div id="tab3" class="tab">
-        <div class="content-block">
-         奖项设置
+        <div id="tab3" class="tab">
+            <div class="content-block">
+                奖项设置
+            </div>
         </div>
-      </div>
-      <div id="tab4" class="tab">
-        <div class="content-block">
-          作品展示
+        <div id="tab4" class="tab">
+            <div class="content-block">
+                作品展示
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+
+    <div class="content-block">
+        <p><a href="#" class="button">Usual Button 1</a></p>
+    </div>
+
 </template>
 <style lang="less">
-  @import "../lib/sui/less/sm.less";
-  .head{
-    height: 420px;
-    max-height: 420px;
-    background-color: #00ffcc;
-  }
+    @import "../lib/sui/less/sm.less";
+
 </style>
 
 
